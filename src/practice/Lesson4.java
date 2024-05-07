3a360a142b03e7008dffed3364eeed782de94497
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-07 11:55:37 +0900

    演習問題（Lesson4）

---
+++ src/practice/Lesson4.java
@@ -0,0 +1,46 @@
+package practice;
+
+class Car {
+	int no;
+	int speed;
+	
+	void setNo(int n) {
+		no = n;
+	}
+	
+	void run(int s) {
+		speed = s;
+	}
+	
+	void stop() {
+		speed = 0;
+	}
+	
+	void display() {
+		System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+	}
+}
+
+public class Lesson4 {
+
+	public static void main(String[] args) {
+		// Carクラスのオブジェクトc1を生成
+		Car c1 = new Car();
+		
+		// c1のナンバーを2525に設定
+		c1.setNo(2525);
+		
+		// c1の速度を30に設定
+		c1.run(30);
+		
+		// c1のナンバー、速度を表示
+		c1.display();
+		
+		// c1の速度を0に設定
+		c1.stop();
+		
+		// c1のナンバー、速度を表示
+		c1.display();
+	}
+
+}
